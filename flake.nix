3bd7ff80d97caa7f6f73dd74f6410d9524dbfaa9
Keshal Desai <keshaldesai@gmail.com>
2026-05-25 16:53:24 -0400

    Add Nix dev shell with Go and protobuf tooling

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "commute helper dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "commute";
+          packages = with pkgs; [
+            go
+            gopls
+            gotools
+            protobuf
+            protoc-gen-go
+          ];
+        };
+      });
+}
